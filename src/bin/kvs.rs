c0374db6bece78434ba06c623c8335de15d9aec8
shane zhang <shanezx@amazon.com>
2023-08-18 20:25:18 -0700

    wip

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,18 +1,64 @@
-use std::env::current_dir;
-use assert_cmd::prelude::OutputOkExt;
-use clap::App;
+use clap::{App, Arg, SubCommand};
 use kvs::KvStore;
+use kvs::KvsError;
 use kvs::Result;
+use std::env::current_dir;
+use std::process::exit;
 
 fn main() -> Result<()> {
     let matches = App::new("kv store")
         .subcommand(
-            Subcommand::with_name("set")
-                .about("Set the key and value")
-                .arg(Arg::with_name("KEY").help("string key").required(true))
-                .arg(Arg::with_name("VALUE")).help("string value").required(true),
+            SubCommand::with_name("set")
+                .about("Set the value of a string key to a string")
+                .arg(Arg::with_name("KEY").help("A string key").required(true))
+                .arg(
+                    Arg::with_name("VALUE")
+                        .help("The string value of the key")
+                        .required(true),
+                ),
         )
-        .get_maches();
-    
-    ok(())
+        .subcommand(
+            SubCommand::with_name("get")
+                .about("Get the value of a string key")
+                .arg(Arg::with_name("KEY").help("A string key").required(true)),
+        )
+        .subcommand(
+            SubCommand::with_name("rm")
+                .about("Remove the value of a string key")
+                .arg(Arg::with_name("KEY").help("A string key").required(true)),
+        )
+        .get_matches();
+
+    match matches.subcommand() {
+        ("set", Some(matches)) => {
+            let key = matches.value_of("KEY").unwrap();
+            let value = matches.value_of("VALUE").unwrap();
+            let mut store = KvStore::open(current_dir()?)?;
+            store.set(key.to_owned(), value.to_owned())?;
+        }
+        ("get", Some(matches)) => {
+            let key = matches.value_of("KEY").unwrap();
+            let store = KvStore::open(current_dir()?)?;
+            if let Some(value) = store.get(key.to_owned())? {
+                println!("{}", value);
+            } else {
+                print!("key not found");
+            };
+        }
+        ("rm", Some(matches)) => {
+            let key = matches.value_of("KEY").unwrap();
+            let mut store = KvStore::open(current_dir()?)?;
+            match store.remove(key.to_owned()) {
+                Ok(_) => {},
+                Err(KvsError::KeyNotFound) => {
+                    println!("key not found");
+                    exit(1);
+                },
+                Err(e) => {return Err(e)},
+            };
+        }
+        _ => unreachable!(),
+    }
+
+    Ok(())
 }
